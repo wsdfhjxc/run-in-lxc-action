4aabaed30725f5b2ff0e9bf5de90a8b15c14ad89
wsdfhjxc <wsdfhjxc@op.pl>
2020-12-15 20:43:15 +0100

    Add test script

---
+++ scripts/test.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "Test command: cat /etc/os-release"
+cat /etc/os-release
+
+echo "Test command: pwd"
+pwd
+
+echo "Test command: ls -la"
+ls -la
+
+echo "Test command: echo 123 > test.txt"
+echo 123 > test.txt
