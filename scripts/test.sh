754dbd04bf2b07741f78b265072612e4ad8846a8
wsdfhjxc <wsdfhjxc@op.pl>
2020-12-20 00:59:06 +0100

    Update test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
-echo "Test command: cat /etc/os-release | head -n 2"
+echo "# cat /etc/os-release | head -n 2"
 cat /etc/os-release | head -n 2; echo
 
-echo "Test command: pwd"
-pwd; echo
-
-echo "Test command: ls -la"
+echo "# ls -la"
 ls -la; echo
 
-echo "Test command: echo \"Success!\" > test.txt"
+echo "# echo \"Success!\" > test.txt"
 echo "Success!" > test.txt
