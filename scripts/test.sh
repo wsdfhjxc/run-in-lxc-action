01cf4e975fc161fc39d2933a2643fd7f7a91378b
wsdfhjxc <wsdfhjxc@op.pl>
2020-12-20 23:47:36 +0100

    Update test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -9,5 +9,5 @@ ls -la
 echo "# echo \"Success!\" > test.txt"
 echo "Success!" > test.txt
 
-echo "# ping -q -c 1 github.com"
-ping -q -c 1 github.com
+echo "# ping -q -c 1 github.com | tail -n 2"
+ping -q -c 1 github.com | tail -n 2
