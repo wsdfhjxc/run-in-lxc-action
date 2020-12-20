54e99593161c03e5acd762bec11335a45a81f692
wsdfhjxc <wsdfhjxc@op.pl>
2020-12-20 02:01:48 +0100

    Update test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 echo "# cat /etc/os-release | head -n 2"
-cat /etc/os-release | head -n 2; echo
+cat /etc/os-release | head -n 2
 
 echo "# ls -la"
-ls -la; echo
+ls -la
 
 echo "# echo \"Success!\" > test.txt"
 echo "Success!" > test.txt
