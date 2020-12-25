8ef0330732f43d6bc65fa55fb8d839f14afe3f9f
wsdfhjxc <wsdfhjxc@op.pl>
2020-12-26 00:25:21 +0100

    Update test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-echo "# grep -w PRETTY_NAME /etc/os-release"
+set -x
+
 grep -w PRETTY_NAME /etc/os-release
 
-echo "# ls -la"
 ls -la
 
-echo "# echo \"Success!\" > test.txt"
-echo "Success!" > test.txt
+echo "Success!" | tee test.txt
